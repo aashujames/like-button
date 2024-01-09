ec675935a757c53f99acb1ca87c4e14b23d02e74
Ashish Gupta <ashishgupt25@gmail.com>
2024-01-09 20:53:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { HeartIcon, SpinnerIcon } from "./icons";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    const [liked, setLiked] = useState(false);
+
+    const handleLikeUnlike = () => {
+        setLiked(!liked);
+    };
+
+    return (
+        <div>
+            <button className={`likeBtn`} onClick={handleLikeUnlike}>
+                <HeartIcon /> {liked ? "Liked" : "Like"}
+            </button>
+        </div>
+    );
+}
+
+export default App;
