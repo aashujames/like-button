bfdc3257d8f6aaad585687672ab4ce9a34ded0a5
Ashish Gupta <ashishgupt25@gmail.com>
2024-01-11 21:03:54 +0530

    feat: handling the api and displaying error

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,35 @@ import "./App.css";
 
 function App() {
     const [liked, setLiked] = useState(false);
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState(null);
 
-    const handleLikeUnlike = () => {
-        setLiked(!liked);
+    const handleLikeUnlike = async () => {
+        setIsFetching(true);
+        setError(null);
+
+        try {
+            const response = await fetch(
+                "https://www.greatfrontend.com/api/questions/like-button",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        action: liked ? "unlike" : "like"
+                    })
+                }
+            );
+
+            if (response.status >= 200 && response.status < 300) {
+                setLiked(!liked);
+            } else {
+                const res = await response.json();
+                setError(res.message);
+                return;
+            }
+        } finally {
+            setIsFetching(false);
+        }
     };
 
     return (
@@ -17,6 +43,7 @@ function App() {
             >
                 <HeartIcon /> {liked ? "Liked" : "Like"}
             </button>
+            {error && <div>{error}</div>}
         </div>
     );
 }
